c91624e045e74fd12d68f38d3b632e998b475294
David Moreno <moreno@rushstreetinteractive.com>
2023-01-06 17:41:28 -0500

    Adding initial config

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// @ts-ignore 
+import reactLogo from './reactLogo.svg';
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <img src={reactLogo} alt="React logo" width="500" />
+      My React app
+    </div>
+  );
+}
+
+export default App;
